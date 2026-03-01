721113282a1dd2e9dac4bba312aad73ef313f137
MOhdDAniShK <0251cse274@niet.co.in>
2026-03-02 02:00:55 +0530

    fix: use bash build script and pin Node 18 for Render deployment

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ socketConfig(server);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('/{*path}', (req, res) =>
+  app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, '../', 'client', 'dist', 'index.html'))
   );
 } else {
